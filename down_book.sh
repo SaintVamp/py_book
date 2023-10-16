e377fc08cbda30d63b651ebd218c2b0636b92b87
admin <saint-wp@163.com>
2023-10-16 17:05:04 +0800

    updated

--- down_book.sh
+++ down_book.sh
@@ -3,7 +3,7 @@ if [ "$hostname" = "R404" ]; then
     mkdir /usr/sv/book/out
     curl -o /usr/sv/book/py_book.py https://gitee.com/saintvamp/py_book/raw/master/run.py
     curl -o /usr/sv/book/book_urls.txt https://gitee.com/saintvamp/py_book/raw/master/book_urls.txt
-    nohup python /usr/sv/book/py_book.py > /usr/sv/out/book/run.log 2>&1 &
+    nohup python /usr/sv/book/py_book.py > /usr/sv/book/out/run.log 2>&1 &
 elif [ "$hostname" = "R2804" ]; then
     echo 'pass'
 fi
