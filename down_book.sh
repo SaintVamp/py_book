d1885470d3d80d8830243cd291037587bf2ed4cc
sv-action <saint-wp@163.com>
2024-02-21 12:25:44 +0000

    merge gitee

--- down_book.sh
+++ down_book.sh
@@ -3,12 +3,12 @@ if [ "$hostname" = "R404" ]; then
     mkdir /usr/sv/book/out
     curl -o /usr/sv/book/py_book.py https://gitee.com/saintvamp/py_book/raw/master/run.py
     curl -o /usr/sv/book/book_urls.txt https://gitee.com/saintvamp/py_book/raw/master/book_urls.txt
-    nohup python /usr/sv/book/py_book.py >> /usr/sv/book/out/run.log 2>&1 &
+    nohup python3 /usr/sv/book/py_book.py >> /usr/sv/book/out/run.log 2>&1 &
 elif [ "$hostname" = "R2804" ]; then
     echo 'pass'
 elif [ "$hostname" = "NASDown" ]; then
     mkdir /data/book/out
     curl -o /data/book/py_book.py https://gitee.com/saintvamp/py_book/raw/master/run.py
     curl -o /data/book/book_urls.txt https://gitee.com/saintvamp/py_book/raw/master/book_urls.txt
-    nohup python /data/book/py_book.py >> /data/book/out/run.log 2>&1 &
+    nohup python3 /data/book/py_book.py >> /data/book/out/run.log 2>&1 &
 fi
